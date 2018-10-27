bcdb35d0d0a36dffbde58ad46d47b2c0d606c014
FrostiBoi <44530249+FrostiBoi@users.noreply.github.com>
2018-10-27 20:00:17 +0300

    Update weapon_zs_nadestick.lua

---
+++ weapon_zs_nadestick.lua
@@ -0,0 +1,185 @@
+SWEP.VElements = {
+	["barrelpump"] = { type = "Model", model = "models/props_c17/oildrum001_explosive.mdl", bone = "ValveBiped.Pump", rel = "", pos = Vector(0.224, 0.8, -2.987), angle = Angle(0, 0, 0), size = Vector(0.157, 0.157, 0.316), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+	["arm"] = { type = "Model", model = "models/props_c17/oildrum001_explosive.mdl", bone = "ValveBiped.Gun", rel = "", pos = Vector(0.086, 1.309, -17.708), angle = Angle(1.131, 0.354, 0), size = Vector(0.101, 0.209, 0.19), color = Color(255, 255, 255, 255), surpresslightning = false, material = "phoenix_storms/Metalfloor_2-3", skin = 0, bodygroup = {} },
+	["grenade1"] = { type = "Model", model = "models/Items/grenadeAmmo.mdl", bone = "ValveBiped.Pump", rel = "", pos = Vector(0.12, -2.597, 3.635), angle = Angle(-1.17, 0, 0), size = Vector(0.5, 0.5, 0.5), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+	["light2"] = { type = "Sprite", sprite = "sprites/animglow02", bone = "ValveBiped.Gun", rel = "", pos = Vector(0.068, -1.772, -7.212), size = { x = 0.352, y = 0.352 }, color = Color(0, 255, 0, 255), nocull = true, additive = true, vertexalpha = true, vertexcolor = true, ignorez = false},
+	["ironsightsplate"] = { type = "Model", model = "models/hunter/plates/platehole2x2.mdl", bone = "ValveBiped.Gun", rel = "", pos = Vector(0.159, -1.926, -7.29), angle = Angle(0, -0.889, 0), size = Vector(0.009, 0.009, 0.204), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+	["boxflare"] = { type = "Model", model = "models/Items/BoxFlares.mdl", bone = "ValveBiped.Gun", rel = "", pos = Vector(-0.185, -0.694, 7.067), angle = Angle(0, 0, 0), size = Vector(0.298, 0.388, 0.358), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+	["chargebattery"] = { type = "Model", model = "models/Items/battery.mdl", bone = "ValveBiped.Gun", rel = "", pos = Vector(0.816, -0.972, -7.68), angle = Angle(-13.867, -3.987, 83.704), size = Vector(0.284, 0.527, 0.284), color = Color(255, 208, 0, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+	["light"] = { type = "Sprite", sprite = "sprites/animglow02", bone = "ValveBiped.Gun", rel = "", pos = Vector(0.982, -2.527, -3.366), size = { x = 1.101, y = 1.101 }, color = Color(255, 0, 0, 255), nocull = true, additive = true, vertexalpha = true, vertexcolor = true, ignorez = false}
+}
+
+SWEP.WElements = {
+	["arm"] = { type = "Model", model = "models/props_c17/oildrum001_explosive.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(10.468, 1.177, -4.792), angle = Angle(83.572, 1.006, 0.321), size = Vector(0.101, 0.101, 0.101), color = Color(255, 255, 255, 255), surpresslightning = false, material = "phoenix_storms/Metalfloor_2-3", skin = 0, bodygroup = {} },
+	["barrelpump"] = { type = "Model", model = "models/props_c17/oildrum001_explosive.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(23.879, 0.944, -5.077), angle = Angle(83.97, -0.584, 0.354), size = Vector(0.119, 0.122, 0.219), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+	["boxflare"] = { type = "Model", model = "models/Items/BoxFlares.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(21.309, 1.009, -6.891), angle = Angle(174.981, 98.549, 85.703), size = Vector(0.298, 0.388, 0.358), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+	["ironsightsplate"] = { type = "Model", model = "models/hunter/plates/platehole2x2.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(11.779, 1.264, -7.177), angle = Angle(75.237, -0.408, -0.779), size = Vector(0.009, 0.009, 0.204), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+	["chargebattery"] = { type = "Model", model = "models/Items/battery.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(12.262, 1.674, -6.362), angle = Angle(1.61, -88.278, -5.335), size = Vector(0.284, 0.527, 0.284), color = Color(255, 208, 0, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+	["grenade1"] = { type = "Model", model = "models/Items/grenadeAmmo.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(17.701, 1.077, -7.449), angle = Angle(1.169, -87.663, -97.014), size = Vector(0.393, 0.393, 0.393), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+	["light"] = { type = "Sprite", sprite = "sprites/animglow02", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(16.027, 1.205, -7.384), size = { x = 1.101, y = 1.101 }, color = Color(255, 0, 0, 255), nocull = true, additive = true, vertexalpha = true, vertexcolor = true, ignorez = false},
+	["light2"] = { type = "Sprite", sprite = "sprites/animglow02", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(11.727, 1.195, -7.264), size = { x = 0.352, y = 0.352 }, color = Color(0, 255, 0, 255), nocull = true, additive = true, vertexalpha = true, vertexcolor = true, ignorez = false}
+}
+ 
+ 
+function SWEP:PrimaryAttack(bNadeType)
+if self:CanPrimaryAttack() then
+		self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
+		self:EmitSound(self.Primary.Sound)
+
+		local clip = self:Clip1()
+
+		self:TakePrimaryAmmo(clip)
+		self.Owner:ViewPunch(clip * 0.5 * self.Primary.Recoil * Angle(math.Rand(-0.1, -0.1), math.Rand(-0.1, 0.1), 0))
+
+		self.Owner:SetGroundEntity(NULL)
+		self.Owner:SetVelocity(-80 * clip * self.Owner:GetAimVector())
+
+		self.IdleAnimation = CurTime() + self:SequenceDuration()
+if SERVER then
+	local nade_class
+	local nade_angle
+	if !bNadeType then
+	nade_class = "projectile_zsgrenade"
+	nade_angle = Angle(90,0,0)
+	else
+	nade_class = "grenade_ar2"
+	nade_angle = Angle(0,0,0)
+	end
+	for i=1,10 do
+	local nade = ents.Create(nade_class)
+	nade:SetPos(self.Owner:GetShootPos() + Vector(math.Rand(5,-5),math.Rand(5,-5),math.Rand(5,-5)))
+	nade:SetAngles(self.Owner:EyeAngles() + nade_angle)
+	nade:Spawn()
+	nade:SetOwner(self.Owner)
+	if !bNadeType then
+	nade:Input("settimer",self.Owner,self.Owner,"1.2")
+	nade:GetPhysicsObject():ApplyForceCenter(self.Owner:GetAimVector()*3500)
+	else
+	nade:SetVelocity(self.Owner:GetAimVector()*1500 + VectorRand()*80)
+	end
+	end
+	end
+    end
+end
+
+
+AddCSLuaFile()
+
+if CLIENT then
+	SWEP.PrintName = "Nadestick"
+	SWEP.Description = "This weapon turns buckshot into live Grenades. Use with caution,m8."
+	SWEP.Slot = 3
+	SWEP.SlotPos = 0
+
+	SWEP.HUD3DBone = "ValveBiped.Gun"
+	SWEP.HUD3DPos = Vector(1.65, 0, -8)
+	SWEP.HUD3DScale = 0.025
+	
+	SWEP.ViewModelFlip = false
+end
+
+SWEP.Base = "weapon_zs_base"
+
+SWEP.HoldType = "shotgun"
+
+SWEP.ViewModel = "models/weapons/c_shotgun.mdl"
+SWEP.WorldModel = "models/weapons/w_shotgun.mdl"
+SWEP.UseHands = true
+
+SWEP.CSMuzzleFlashes = true
+
+SWEP.ReloadDelay = 0.8
+
+SWEP.Primary.Sound = Sound("ambient/explosions/explode_4.wav")
+SWEP.Primary.Recoil = 12.5
+SWEP.Primary.Damage = 36
+SWEP.Primary.NumShots = 6
+SWEP.Primary.Delay = 1.5
+
+SWEP.Primary.ClipSize = 1
+SWEP.Primary.Automatic = false
+SWEP.Primary.Ammo = "buckshot"
+SWEP.Primary.DefaultClip = 28
+
+SWEP.ConeMax = 0.23
+SWEP.ConeMin = 0.2
+
+SWEP.WalkSpeed = SPEED_SLOWER
+
+function SWEP:SetIronsights()
+end
+
+SWEP.reloadtimer = 0
+SWEP.nextreloadfinish = 0
+
+function SWEP:Reload()
+	if self.reloading then return end
+
+	if self:GetNextReload() <= CurTime() and self:Clip1() < self.Primary.ClipSize and 0 < self.Owner:GetAmmoCount(self.Primary.Ammo) then
+		self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
+		self.reloading = true
+		self.reloadtimer = CurTime() + self.ReloadDelay
+		self:SendWeaponAnim(ACT_SHOTGUN_RELOAD_START)
+		self.Owner:DoReloadEvent()
+		self:SetNextReload(CurTime() + self:SequenceDuration())
+	end
+end
+
+
+function SWEP:Think()
+	if self.reloading and self.reloadtimer < CurTime() then
+		self.reloadtimer = CurTime() + self.ReloadDelay
+		self:SendWeaponAnim(ACT_VM_RELOAD)
+
+		self.Owner:RemoveAmmo(1, self.Primary.Ammo, false)
+		self:SetClip1(self:Clip1() + 1)
+		self:EmitSound("Weapon_Shotgun.Reload")
+
+		if self.Primary.ClipSize <= self:Clip1() or self.Owner:GetAmmoCount(self.Primary.Ammo) <= 0 or not self.Owner:KeyDown(IN_RELOAD) then
+			self.nextreloadfinish = CurTime() + self.ReloadDelay
+			self.reloading = false
+			self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
+		end
+	end
+
+	local nextreloadfinish = self.nextreloadfinish
+	if nextreloadfinish ~= 0 and nextreloadfinish < CurTime() then
+		self:SendWeaponAnim(ACT_SHOTGUN_PUMP)
+		self:EmitSound("Weapon_Shotgun.Special1")
+		self.nextreloadfinish = 0
+	end
+
+	if self.IdleAnimation and self.IdleAnimation <= CurTime() then
+		self.IdleAnimation = nil
+		self:SendWeaponAnim(ACT_VM_IDLE)
+	end
+
+	if self:GetIronsights() and not self.Owner:KeyDown(IN_ATTACK2) then
+		self:SetIronsights(false)
+	end
+end
+function SWEP:CanPrimaryAttack()
+	if self.Owner:IsHolding() or self.Owner:GetBarricadeGhosting() then return false end
+
+	if self:Clip1() <= 0 then
+		self:EmitSound("Weapon_Shotgun.Empty")
+		self:SetNextPrimaryFire(CurTime() + 0.25)
+		return false
+	end
+
+	if self.reloading then
+		if self:Clip1() < self.Primary.ClipSize then
+			self:SendWeaponAnim(ACT_SHOTGUN_RELOAD_FINISH)
+		else
+			self:SendWeaponAnim(ACT_SHOTGUN_PUMP)
+			self:EmitSound("Weapon_Shotgun.Special1")
+		end
+		self.reloading = false
+		self:SetNextPrimaryFire(CurTime() + 0.25)
+		return false
+	end
+
+	return self:GetNextPrimaryFire() <= CurTime()
+end
+
+
